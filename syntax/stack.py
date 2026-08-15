dae743d8099ea96cb7e0056f087b93eba2a8c62b
Ayush Mishra <ayushmishra14055@gmail.com>
2026-08-15 22:31:18 +0530

    Add stack solution

---
+++ syntax/stack.py
@@ -0,0 +1,13 @@
+#LIFO
+
+stack = [] #create an empty stack
+
+stack.append(1) #push an element onto the stack
+
+stack.append(2) #push another element onto the stack
+
+stack.pop() #pop the top element from the stack
+
+stack[-1] #access the top element of the stack without removing it
+
+len(stack) #get the number of elements in the stack
