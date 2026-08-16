20eb9fd2eb2da654889dc8f089fbe9cd54f5bfd8
Ayush <ayushmishra14055@gmail.com>
2026-08-16 23:20:21 +0530

    Create hashmap.py

---
+++ syntax/hashmap.py
@@ -0,0 +1,6 @@
+d = {1: "one", 2: "two", 3: "three"}    #create
+d["four"] = 4                                #insert
+d[2]                                         #access
+d[1] = "ONE"                                   #update
+del d[3]                                      #delete
+len(d)                                        #size
